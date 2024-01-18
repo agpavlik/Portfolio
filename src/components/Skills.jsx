bf03c27a2706a3de094631969c13c24558eedf01
agpavlik <agpavlik@gmail.com>
2024-01-18 12:06:41 -0500

    Implement Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { skills } from "../data";
+import { softskills } from "../data";
+import { BsCheck2All } from "react-icons/bs";
 
 function Skills() {
   return (
@@ -12,7 +14,7 @@ function Skills() {
         {skills.map((skill, index) => (
           <div
             key={index}
-            className="flex gap-4 shadow-lg py-2 px-6 bg-[#04133e] rounded-full items-center hover:animate-bounce ease-in-out duration-300"
+            className="flex gap-4 shadow-lg py-2 px-6 bg-[#04133e] rounded-full items-center hover:animate-bounce ease-in-out duration-3 00"
           >
             <div className="w-10 h-10">
               <img src={skill.icon} className="w-full h-full rounded-full" />
@@ -35,6 +37,31 @@ function Skills() {
             </div>
           </div>
         ))}
+      </div>
+      <div className="mt-[10rem] ">
+        <h4 className="text-3xl font-bold text-black dark:text-white mb-14 text-center">
+          Soft Skills
+        </h4>
+
+        <div className="flex flex-col md:flex-row gap-16 items-center justify-center">
+          <div className="flex flex-col bg-[#000000] dark:bg-[#224cff10] p-6 rounded-xl shadow-xl scale-110 hover:scale-125 ease-in-out duration-500 mb-10 md:mb-0">
+            {softskills.slice(0, 5).map((s, index) => (
+              <div key={index + s} className="flex items-center gap-3 p-4">
+                <BsCheck2All color="white" size={22} />
+                <p className="text-white">{s}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex flex-col bg-[#000000] dark:bg-[#224cff10] p-6 rounded-xl shadow-xl scale-110 hover:scale-125 ease-in-out duration-500 mb-10 md:mb-0">
+            {softskills.slice(5, 10).map((s, index) => (
+              <div key={index + s} className="flex items-center gap-3 p-4">
+                <BsCheck2All color="white" size={22} />
+                <p className="text-white">{s}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
