9321edaef5d0ecc0ab1843889fdfd223aef816ee
agpavlik <agpavlik@gmail.com>
2024-01-17 23:29:23 -0500

    Initialize About

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import About from "./components/About";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -33,6 +34,12 @@ function App() {
         </div>
         <section className="w-full bg-[#061130] py-20 ">
           <Projects />
+        </section>
+        <section
+          id="about"
+          className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#030a1c] to-[#05174e]"
+        >
+          <About />
         </section>
       </div>
     </div>
